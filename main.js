abd228fb2cbffb621ebf960ea810685562731257
Francesco Maestrelli <framae26@gmail.com>
2022-02-02 15:41:59 +0100

    prima push

--- main.js
+++ main.js
@@ -1,30 +1,55 @@
+// const items = [
+//     {
+//         photo: 'img/01.jpg',
+//         title: "Svezia",
+//         text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
+//     },
+//     {
+//         photo: 'img/02.jpg',
+//         title: "Svizzera",
+//         text: "Lorem ipsum."
+//     },
+//     {
+//         photo: 'img/03.jpg',
+//         title: "Gran Bretagna",
+//         text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+//     },
+//     {
+//         photo: 'img/04.jpg',
+//         title: "Germania",
+//         text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+//     },
+//     {
+//         photo: 'img/05.jpg',
+//         title: "Paradise",
+//         text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+//     }
+// ];
+
 const items = [
-    {
-        photo: 'img/01.jpg',
-        title: "Svezia",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
-    },
-    {
-        photo: 'img/02.jpg',
-        title: "Svizzera",
-        text: "Lorem ipsum."
-    },
-    {
-        photo: 'img/03.jpg',
-        title: "Gran Bretagna",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
-    },
-    {
-        photo: 'img/04.jpg',
-        title: "Germania",
-        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
-    },
-    {
-        photo: 'img/05.jpg',
-        title: "Paradise",
-        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
-    }
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
 ];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
 
 const itemsRef = document.getElementsByClassName('items')[0];
 const thumbsRef = document.getElementsByClassName('thumbs')[0];
